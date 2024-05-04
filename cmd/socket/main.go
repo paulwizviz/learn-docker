abe80bebd5ba1aca4787d20776c5991aef14fd24
Paul Sitoh <paulwizviz@gmail.com>
2024-05-04 15:53:21 +0100

    Switch to slog
    To disabiguate info from error.
    
    Signed-off-by: Paul Sitoh <paulwizviz@gmail.com>

--- cmd/socket/main.go
+++ cmd/socket/main.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"io/fs"
-	"log"
 	"log/slog"
 	"net"
 	"net/http"
@@ -17,13 +16,13 @@ func processConn(c net.Conn) {
 	buf := make([]byte, 1024)
 	nr, err := c.Read(buf)
 	if err != nil {
-		log.Fatalf("Unable to read. Reason: %v", err)
+		slog.Info(fmt.Sprintf("Unable to read. Reason: %v", err))
+		os.Exit(1)
 	}
-	fmt.Printf("Received: %s\n", string(buf[:nr]))
+	slog.Info(fmt.Sprintf("Received: %s\n", string(buf[:nr])))
 }
 
 func server(socket string) error {
-
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
@@ -35,42 +34,33 @@ func server(socket string) error {
 	// socket listening
 	l, err := net.Listen("unix", socket)
 	if err != nil {
-		log.Fatalf("Unable to start server. Reason: %s", err.Error())
+		slog.Info(fmt.Sprintf("Unable to start server. Reason: %v", err))
+		os.Exit(1)
 	}
 	defer os.Remove(socket)
 
 	for {
 		fd, err := l.Accept()
 		if err != nil {
-			log.Fatalf("Error: %s", err.Error())
+			slog.Info(fmt.Sprintf("Error: %v", err))
+			os.Exit(1)
 		}
 		processConn(fd)
 	}
 }
 
 var isServer bool
-var logFile string
-var socketFile string
 
 func main() {
 	os.Mkdir("/var/socket", fs.ModePerm)
-	flag.StringVar(&logFile, "log", "/var/socket/app.log", "name of logfile")
-	flag.StringVar(&socketFile, "socket", "/var/socket/app.socket", "socket file")
+	socketFile := "/var/socket/app.socket"
 	flag.BoolVar(&isServer, "server", true, "server mode")
 	flag.Parse()
 
-	l, err := os.Create(logFile)
-	if err != nil {
-		os.Exit(1)
-	}
-
-	logger := slog.New(slog.NewJSONHandler(l, nil))
-	slog.SetDefault(logger)
-
 	if isServer {
-		err = server(socketFile)
+		err := server(socketFile)
 		if err != nil {
-			slog.Info("Socket unable of set")
+			slog.Error(fmt.Sprintf("Server error: %v", err))
 		}
 	} else {
 		count := 0
@@ -79,9 +69,9 @@ func main() {
 			slog.Info(fmt.Sprintf("Request host: %v", req.Host))
 			conn, err := net.Dial("unix", socketFile)
 			if err != nil {
+				slog.Error(fmt.Sprintf("Server error: %v", err))
 				rw.WriteHeader(http.StatusExpectationFailed)
 				rw.Write([]byte("Fail to connect to socket"))
-				slog.Info("Fail to connect to socket")
 				return
 			}
 
@@ -89,9 +79,9 @@ func main() {
 			msg := fmt.Sprintf("%v-Hello", count)
 			_, err = conn.Write([]byte(msg))
 			if err != nil {
+				slog.Error(fmt.Sprintf("Server error: %v", err))
 				rw.WriteHeader(http.StatusExpectationFailed)
 				rw.Write([]byte("Fail to connect to socket"))
-				slog.Info("Fail to connect to socket")
 				return
 			}
 			rw.WriteHeader(http.StatusOK)
@@ -99,6 +89,9 @@ func main() {
 		})
 		port := 9090
 		fmt.Printf("REST server started on port: %v", port)
-		log.Fatal(http.ListenAndServe(fmt.Sprintf("0.0.0.0:%v", port), mux))
+		err := http.ListenAndServe(fmt.Sprintf("0.0.0.0:%v", port), mux)
+		if err != nil {
+			slog.Error(fmt.Sprintf("Server error: %v", err))
+		}
 	}
 }
